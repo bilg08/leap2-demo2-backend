025be95630af6ce1df9c7f9447a9025ec2d1608d
bilguunLeap2 <bilguunleap2@KEKW.local>
2022-11-18 09:53:16 +0800

    backend init

--- router/chat.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { sendChat, getChats } = require("../controller/chat");
-const { checkAccessToken } = require("../middleware/checkAccessToken");
-router.route("/:chatRoomName/sendMessage").post(checkAccessToken,sendChat);
-router.route("/:chatRoomName/getMessages").get(checkAccessToken,getChats);
-
-module.exports = router;
