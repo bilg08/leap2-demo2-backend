7bf328be68e6f49fba0be9db09bb1f315b009fd6
bilguunLeap2 <bilguunleap2@KEKW.local>
2022-11-22 09:16:08 +0800

    backend

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 const errorHandler = require("./middleware/error");
 const http = require("http").Server(app);
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://student-assignment-project-front-g87zala5p-bilg08.vercel.app",
 };
 app.use(cors(corsOptions));
 
@@ -20,7 +20,7 @@ dotenv.config({ path: "./config.env" });
 
 const socketIO = require("socket.io")(http, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://student-assignment-project-front-g87zala5p-bilg08.vercel.app",
   },
 });
 
